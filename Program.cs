cc74c343b891adbbfd2f3e93bf57de56a81fd255
Arkadiusz Ryszewski <arkadiusz.ryszewski@sollers.eu>
2019-04-09 17:06:10 +0200

    Program structure

--- Program.cs
+++ Program.cs
@@ -10,8 +10,29 @@ namespace Rekonstrukcja
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
-            Console.Read();
+            Console.WriteLine("Tree reconstruction");
+            int[,] input = readInput();
+            int[,] result = findTree(input);
+            outputResult(result);
+        }
+
+        static int[,] readInput()
+        {
+            // TODO: read input from file
+            // TODO: display input
+            return new int[10, 10];
+        }
+
+        static int[,] findTree(int[,] input)
+        {
+            // TODO: perform calculations
+            return input;
+        }
+
+        static void outputResult(int[,] result)
+        {
+            // TODO: display result
+            // TODO: save result to the file
         }
     }
 }
