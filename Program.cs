85815f79441b77083c9e73fb089c20cd45397572
Arkadiusz Ryszewski <arkadiusz.ryszewski@sollers.eu>
2019-04-09 17:32:27 +0200

    read & display input

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,17 +11,35 @@ namespace Rekonstrukcja
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Tree reconstruction");
+            Console.WriteLine("Tree reconstruction\n");
             int[,] input = readInput();
             int[,] result = findTree(input);
             outputResult(result);
+            Console.Read();
         }
 
         static int[,] readInput()
-        {
-            // TODO: read input from file
-            // TODO: display input
-            return new int[10, 10];
+        { 
+            string[] lines = File.ReadAllLines(".\\..\\..\\input.txt");
+            int n = lines.Length;
+            var result = new int[n, n];
+            int longestNumberLength = 0;
+            for (var i = 0; i < n; i++)
+            {
+                var valuesInRow = lines[i].Split(';');
+                for (var j = 0; j < n; j++)
+                {
+                    if (valuesInRow[j].Length > longestNumberLength)
+                    {
+                        longestNumberLength = valuesInRow[j].Length;
+                    }
+                    result[i, j] = int.Parse(valuesInRow[j]);
+                }
+            }
+
+            Console.WriteLine("Input adjacency matrix:");
+            Utils.DisplayMatrix(result, longestNumberLength);
+            return result;
         }
 
         static int[,] findTree(int[,] input)
---
+++ Utils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rekonstrukcja
+{
+    class Utils
+    {    
+
+        public static void DisplayMatrix(int[,] matrix, int longestNumberLength)
+        {
+            int n = (int) Math.Sqrt(matrix.Length);
+            for (var i = 0; i < n; i++)
+            {
+                Console.Write("| ");
+                for (var j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i, j]);
+                    for (var k = 0; k < longestNumberLength - NumberLength(matrix[i, j]) + 1; k++)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine("|");
+            }
+        }
+
+        private static int NumberLength(int number)
+        {
+            return number.ToString().Length;
+        }
+
+    }
+}
