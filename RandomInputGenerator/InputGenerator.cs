4c6a45584438389ebfb32c4591648efb3be695bb
Stanisław Wasilkowski <wasilkowskis@student.mini.pw.edu.pl>
2019-05-10 22:30:07 +0200

    Add random input generator

---
+++ RandomInputGenerator/InputGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace RandomInputGenerator
+{
+    internal class InputGenerator
+    {
+        internal static void GenerateRandomInput(int matrixSize, string filename)
+        {
+            var random = new Random();
+            var matrix = new int[matrixSize, matrixSize];
+            var distanceToCenter = new int[matrixSize];
+
+            for (int i = 0; i < matrixSize; i++)
+            {
+                distanceToCenter[i] = random.Next(matrixSize) + 1;
+            }
+
+            for (int i = 0; i < matrixSize; i++)
+            {
+                for (int j = i; j < matrixSize; j++)
+                {
+                    if (i == j)
+                    {
+                        matrix[i, i] = 0;
+                    }
+                    else
+                    {
+                        matrix[i, j] = matrix[j, i] = distanceToCenter[i] + distanceToCenter[j];
+                    }
+                }
+            }
+
+            using (var stream = new FileStream(filename, FileMode.Create))
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    for (int i = 0; i < matrixSize; i++)
+                    {
+                        for (int j = 0; j < matrixSize; j++)
+                        {
+                            writer.Write(matrix[i, j]);
+                            if (j < matrixSize - 1)
+                            {
+                                writer.Write(';');
+                            }
+                        }
+                        writer.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ RandomInputGenerator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomInputGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrixSize = 5;
+            var fileName = "input.txt";
+            if (args.Length > 0)
+            {
+                try
+                {
+                    matrixSize = int.Parse(args[0]);
+                    fileName = args[1];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Incorrect number of arguments.");
+                    return;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Wrong arguments format.");
+                    return;
+                }
+            }
+            InputGenerator.GenerateRandomInput(matrixSize, fileName);
+        }
+    }
+}
